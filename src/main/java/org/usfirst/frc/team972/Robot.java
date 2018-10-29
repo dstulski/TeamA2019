b87cb6660e4a5e837f5509410adeeac87720fdec
dstulski <35385060+dstulski@users.noreply.github.com>
2018-10-29 16:23:24 -0700

    Update Robot.java

--- src/main/java/org/usfirst/frc/team972/Robot.java
+++ src/main/java/org/usfirst/frc/team972/Robot.java
@@ -2,7 +2,12 @@ package org.usfirst.frc.team972;
 import edu.wpi.first.wpilibj.*;
 
 public class Robot extends IterativeRobot {
-	
-	
-
-}
+	WPI_TalonSRX 10%OfPeople = new WPI_TalonSRX();
+	WPI_TalonSRX 90%OfPeople = new WPI_TalonSRX();
+	Joystick christmasTree = new Joystick;
+	public void TeleopTankDrive() {
+		double leftHand = christmasTree.getRawAxis;
+		double rightHand = christmasTree.getRawAxis;
+		
+		System.out.println("Left hand strength: " + leftHand);
+		System.out.println("Right hand strength: " + rightHand);
